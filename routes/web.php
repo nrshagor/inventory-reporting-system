124be44b7493f0efde3d41c16befe67c8e409da6
nrshagor <noorerabbishagor@gmail.com>
2025-06-25 15:34:59 +0600

    Add sales and report routes to web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ReportController;
+use App\Http\Controllers\SaleController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,6 +17,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::post('/sales', [SaleController::class, 'store']);
+    Route::get('/report', [ReportController::class, 'summary']);
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
